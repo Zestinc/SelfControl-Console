ada95e90ba5f844223c81a695ffa9b5ed5c81a52
zestinc <zestinc32@gmail.com>
2015-09-17 14:40:38 -0700

    Initial Commit

---
+++ SelfControl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SelfControl
+//
+//  Created by Zestinc on 15/9/17.
+//  Copyright (c) 2015年 Zestinc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
